8744050e3c7b345e7d3c26bf623a90390dff1d41
Sizzle <93177291+Sizzle-Z@users.noreply.github.com>
2021-12-17 16:51:38 -0800

    Update rps.py

--- rps.py
+++ rps.py
@@ -9,7 +9,7 @@ user_selection = input('Select rock, paper, or sissors! ')
 user_selection_lower = user_selection.lower()
 
 # Check who won
-if ai_selection == user_selection: print('There was a tie!')
+if ai_selection == user_selection_lower: print('There was a tie!')
 if ai_selection == 'Rock' and user_selection_lower == 'sissors': print('You lost! The AI chose ' + ai_selection)
 if ai_selection == 'Sissors' and user_selection_lower == 'rock': print('You won! The AI chose ' + ai_selection)
 if ai_selection == 'Sissors' and user_selection_lower == 'paper': print('You lost! The AI chose ' + ai_selection)
